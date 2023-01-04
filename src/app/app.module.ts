e82f86ceaf45933b63c0e8f1949e28baaf9c0cf9
Antonia Ekdahl <antoniaekdahl@gmail.com>
2023-01-04 10:19:36 +0100

    Add generated component, service and datatype files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RandomActivityComponent } from './random-activity/random-activity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RandomActivityComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/random-activity/random-activity.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-random-activity',
+  templateUrl: './random-activity.component.html',
+  styleUrls: ['./random-activity.component.css']
+})
+export class RandomActivityComponent {
+
+}
